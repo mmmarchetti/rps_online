272c4fd632e37a9fa1c12e44159c5f276702e07d
marchetti-icts <marcos.marchetti@icts.com.br>
2023-03-21 18:03:21 -0300

    refactored models.py

--- models.py
+++ models.py
@@ -1,23 +1,92 @@
-import bcrypt
 from flask import request, session, redirect, url_for, flash
+from typing import Dict, Union, Optional
+from werkzeug import Response
 from database import users
+import bcrypt
 import uuid
 
 
 class User:
 
-    def start_session(self, user):
-        session['logged_in'] = True
-        session['userid'] = user["_id"]
-        session["username"] = user["username"]
-        return redirect('/profile/'+user["username"])
+    def __init__(self):
+        self.session = session
 
-    def signup(self):
+    def start_session(self, user: Dict[str, str]) -> redirect:
+        """
+        Start a new session for the given user.
+
+        Args:
+            user (dict): A dictionary containing user information, including the user's ID and username.
+
+        Returns:
+            flask.redirect: A redirect to the user's profile page.
+        """
+        self.session['logged_in'] = True
+        self.session['userid'] = user["_id"]
+        self.session["username"] = user["username"]
+
+        return redirect(f'/profile/{user["username"]}')
+
+    @staticmethod
+    def search_db_available(db_search_type: str, user_search_type: str) -> bool:
+        """
+            Check if a given user search type value is available in the database for the given search type.
+
+            Args:
+                db_search_type (str): The search type to use for the database query.
+                user_search_type (str): The value to search for in the database for the given search type.
+
+            Returns:
+                bool: True if the user search type value is not found in the database for the given search type,
+                False otherwise.
+            """
+
+        return users.find_one({db_search_type: user_search_type}) is None
+
+    @staticmethod
+    def is_available(type_check_available: bool, type_of_check: str) -> Union[None, redirect]:
+        """
+        Check if the given type is available. If it's not, a flash message is added, and the user is redirected to the
+        signup page.
+
+        Args:
+            type_check_available (bool): A boolean that indicates whether the given type is available or not.
+            type_of_check (str): A string indicating the type being checked.
+
+        Returns:
+            Union[None, redirect]: None if the type is available, else a redirect to the signup page.
+        """
+        if not type_check_available:
+            flash(f"{type_of_check} already in use")
+            return redirect(url_for('signup_page'))
+
+    @staticmethod
+    def check_password() -> redirect:
+        """
+        Check if password and confirm_password fields match.
+
+        Returns:
+            flask.redirect: A redirect to the signup page if passwords do not match.
+        """
+        if request.form.get('password') != request.form.get('confirm_password'):
+            flash("Passwords do not match")
+            return redirect(url_for('signup_page'))
+
+    def signup(self) -> Response:
+        """
+        Sign up a new user by adding their information to the database.
+
+        Returns:
+            Union[flash, redirect]: A flash message if the sign-up is unsuccessful,
+            or a redirect to the login page if the sign-up is successful.
+        """
+
         salt = bcrypt.gensalt()
 
         # Usernames that contain '/' causes a bug, need to add a check for that
-        is_valid_username = '/' not in request.form.get('username')
-        if not is_valid_username:
+        # Check if the username is valid
+        username = request.form.get('username')
+        if '/' in username:
             flash("Usernames cannot contain '/' !")
             return redirect(url_for('signup_page'))
 
@@ -33,38 +102,44 @@ class User:
         }
 
         if len(list(users.find({}))) > 0:
-            is_available_email = users.find_one({"email": user['email']}) is None
-            is_available_name = users.find_one({"username": user['username']}) is None
+            # Check if Name and/or Email exists
+            is_available_email = self.search_db_available("email", user['email'])
+            self.is_available(is_available_email, "Email")
 
-            # Check for existing email address
-            if not is_available_email:
-                flash("Email address already in use")
-                return redirect(url_for('signup_page'))
+            is_available_name = self.search_db_available("username", user['username'])
+            self.is_available(is_available_name, "Name")
 
-            if not is_available_name:
-                flash("Username already in use")
-                return redirect(url_for('signup_page'))
-
-        if request.form.get('password') != request.form.get('confirm_password'):
-            flash("password are not matched")
-            return redirect(url_for('signup_page'))
+        self.check_password()
 
         users.insert_one(user)
-        print(list(users.find()))
+
         return redirect(url_for('login_page'))
 
-    def signout(self):
-        session.clear()
-        flash("Sucessfully signed out!")
+    def signout(self) -> redirect:
+        """
+        Clears the user's session and redirects to the homepage.
+
+        Returns:
+            flask.redirect: A redirect to the homepage.
+        """
+        self.session.clear()
+        flash("Successfully signed out!")
         return redirect('/')
 
-    def login(self):
+    def login(self) -> Optional[redirect]:
+        """
+        Log in the user with the email and password from the request form.
 
-        if len(list(users.find({}))) > 0:
-            user_found: dict = users.find_one({"email": request.form.get('email')})
+        Returns:
+            Optional[flask.redirect]: A redirect to the user's profile page if login successful,
+            otherwise a redirect to the home page.
+        """
+        user_list = list(users.find({}))
+        if user_list:
+            user_found = users.find_one({"email": request.form.get('email')})
 
-            if user_found and bcrypt.hashpw(request.form.get('password').encode(),
-                                            user_found['salt']) == user_found['password']:
+            if user_found and bcrypt.checkpw(request.form.get('password').encode(),
+                                             user_found['password'].encode()):
                 return self.start_session(user_found)
 
         flash("Can't login due to wrong password or invalid email.")
