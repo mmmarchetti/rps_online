0d662536ab5d68b92e2076885fefb435990c72ca
Marcos Martins Marchetti <marcosmartins.marchetti@gmail.com>
2023-04-27 15:29:21 +0000

    refactored javascript code

--- static/socketio.js
+++ static/socketio.js
@@ -1,72 +1,82 @@
+/**
+ * Main function to initialize the game.
+ */
 document.addEventListener('DOMContentLoaded', () => {
-
     // Connect to websocket
-    var socket = io.connect(
-        location.protocol + '//' + document.domain + ':' + location.port, {transports: ['websocket']}
-    );
-
-    let player_room_id = false;
+    const socket = io.connect(`${location.protocol}//${document.domain}:${location.port}`, { transports: ['websocket'] });
+  
+    let playerRoomId = false;
     let player1 = false;
     let player2 = false;
     let maria = false;
-
+  
+    // Request to start the game
     socket.emit('start_game');
-
+  
+    /**
+     * Handle alert events from the server.
+     */
     socket.on('alert', data => {
-        window.alert(data['message']);
+      window.alert(data.message);
     });
-
+  
+    /**
+     * Set player information received from the server.
+     */
     socket.on('send_info_player_event', data => {
-        player_room_id = data['player_room_id'];
-        player1 = data['player1'];
-        player2 = data['player2'];
+      playerRoomId = data.player_room_id;
+      player1 = data.player1;
+      player2 = data.player2;
     });
-
+  
+    /**
+     * Show game event from server.
+     */
     socket.on('show_game_event', data => {
-
-        window.alert("Game Started!");
-
-        document.getElementsByClassName("game")[0].style.visibility = 'visible';
-        const message = document.querySelector('#message');
-        message.innerHTML = "Game Started! " + player1 + " VS " + player2;
-
-        const name2 = document.querySelector('.name2');
-        name2.innerHTML = player2;
+      window.alert('Game Started!');
+  
+      document.querySelector('.game').style.visibility = 'visible';
+      document.querySelector('#message').innerHTML = `Game Started! ${player1} VS ${player2}`;
+      document.querySelector('.name2').innerHTML = player2;
     });
-
-    socket.on('wait', data =>{
-        document.getElementById("bottom_message").innerHTML = data['person_waiting'] + " is waiting...";
+  
+    /**
+     * Update the waiting message.
+     */
+    socket.on('wait', data => {
+      document.querySelector('#bottom_message').innerHTML = `${data.person_waiting} is waiting...`;
     });
-
+  
+    /**
+     * Update the opponent's choice when received from the server.
+     */
     socket.on('update_opponent_choice', data => {
-        let opponent;
-        
-        if (username === player2) {
-            opponent = 'player1';
-        }else{
-            opponent = 'player2';
-        }
-
-        const opponent_choice = data['choices'][opponent];
-        
-        setChoiceImage(opponent, opponent_choice);
+      const opponent = (username === player2) ? 'player1' : 'player2';
+      const opponentChoice = data.choices[opponent];
+  
+      setChoiceImage(opponent, opponentChoice);
     });
-
-
+  
+    /**
+     * Set the image for the player's choice.
+     *
+     * @param {string} player - Player identifier ('player1' or 'player2').
+     * @param {string} choice - Choice of the player ('rock', 'paper', or 'scissor').
+     */
     function setChoiceImage(player, choice) {
       const imagePath = `static/images/${choice}.png`;
-      const playerChoiceImage = document.getElementById(`${player}_choice`);
-      playerChoiceImage.src = imagePath;
+      document.querySelector(`#${player}_choice`).src = imagePath;
     }
-
-
+  
+      /**
+     * Handle the result of the game received from the server.
+     */
     socket.on('result', data => {
-        let message = ""
+        let message = '';
 
-        if (data['result'] === 'TIE'){
-            message = "It's a tie!"
-            document.getElementById("bottom_message").innerHTML = message;
-        }else{
+        if (data.result === 'TIE') {
+        message = "It's a tie!";
+        } else {
             if (data['result'] === 'player1'){
                 const winner = document.getElementsByClassName("name1")[0].innerHTML
                 message = winner + " won!"
@@ -81,143 +91,88 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
 
+        document.querySelector('#bottom_message').innerHTML = message;
         window.alert(message);
 
         setTimeout(() => {
-            setChoiceImage('player1', 'logo');
-            setChoiceImage('player2', 'logo');
+        setChoiceImage('player1', 'logo');
+        setChoiceImage('player2', 'logo');
         }, 3000);
-
     });
 
+  
+    /**
+     * Handle leave room button click event.
+     */
     document.querySelector('#leave_room_btn').onclick = () => {
-            let player;
-            const confirmed = window.confirm('Are you sure you want to leave the room?');
-
-            if (confirmed) {
-                if (username === player1) {
-                    player = 'player1';
-                }else{
-                    player = 'player2';
-                }
-                socket.emit('leave_game_page', {'player': player, 'player_room_id': player_room_id});
-            }
+      const confirmed = window.confirm('Are you sure you want to leave the room?');
+  
+      if (confirmed) {
+        const player = (username === player1) ? 'player1' : 'player2';
+        socket.emit('leave_game_page', { player, player_room_id: playerRoomId });
+      }
     }
-
-
+  
+    /**
+     * Clear the game when the room is closed.
+     */
     socket.on('clear_game_event', data => {
-
-        document.getElementById("player1_score").innerHTML = '0';
-        document.getElementById("player2_score").innerHTML = '0';
-        document.getElementById("message").innerHTML = "The game room as closed.";
-        document.getElementById("game_room_id").innerHTML = "";
-        document.getElementById("bottom_message").innerHTML = "";
-        document.getElementsByClassName("game")[0].style.visibility = 'hidden';
-        document.getElementsByClassName("go_to_lobby")[0].style.visibility = 'visible';
-
-        player_room_id = false;
-        player1 = false;
-        player2 = false;
-        maria = false;
-
-        window.alert(`The ${data['player']} left the room.`);
+      document.querySelector('#player1_score').innerHTML = '0';
+      document.querySelector('#player2_score').innerHTML = '0';
+      document.querySelector('#message').innerHTML = 'The game room has closed.';
+      document.querySelector('#game_room_id').innerHTML = '';
+      document.querySelector('#bottom_message').innerHTML = '';
+      document.querySelector('.game').style.visibility = 'hidden';
+      document.querySelector('.go_to_lobby').style.visibility = 'visible';
+  
+      playerRoomId = false;
+      player1 = false;
+      player2 = false;
+      maria = false;
+  
+      window.alert(`${data.player} left the room.`);
     });
-
-
-
+  
+    /**
+     * Handle the click event for rock choice.
+     */
     document.querySelector('#rock').onclick = () => {
-        let player_number;
-
-        if (player1 === username) {
-            player_number = "player1";
-
-        } else {
-            player_number = "player2";
-            
-        }
-        
-        setChoiceImage(player_number, 'rock');
-
-        if (maria === true){
-            socket.emit('register_player_choice', {
-                'player_number': player_number,
-                'player1':player1,
-                'player2':'MarIA',
-                'choice':'rock',
-                'player_room_id':player_room_id});
-
-        }else {
-        socket.emit('register_player_choice', {
-            'player_number': player_number,
-            'player1': player1,
-            'player2': player2,
-            'choice': 'rock',
-            'player_room_id': player_room_id
-        });
-        }
+      handleChoiceClick('rock');
     }
-
+  
+    /**
+     * Handle the click event for paper choice.
+     */
     document.querySelector('#paper').onclick = () => {
-        let player_number;
-
-        if (player1 === username) {
-            player_number = "player1";
-            
-        } else {
-            player_number = "player2";
-            
-        }
-
-        setChoiceImage(player_number, 'paper');
-
-        if (maria === true){
-            socket.emit('register_player_choice', {
-                'player_number': player_number,
-                'player1':player1,
-                'player2':'MarIA',
-                'choice':'paper',
-                'player_room_id':player_room_id});
-
-        }else {
-        socket.emit('register_player_choice', {
-            'player_number': player_number,
-            'player1': player1,
-            'player2': player2,
-            'choice': 'paper',
-            'player_room_id': player_room_id
-        });
-        }
+      handleChoiceClick('paper');
     }
-
+  
+    /**
+     * Handle the click event for scissor choice.
+     */
     document.querySelector('#scissor').onclick = () => {
-        let player_number;
-
-        if (player1 === username) {
-            player_number = "player1";
-            
-        } else {
-            player_number = "player2";
-            
-        }
-
-        setChoiceImage(player_number, 'scissor');
-
-        if (maria === true){
-            socket.emit('register_player_choice', {
-                'player_number': player_number,
-                'player1':player1,
-                'player2':'MarIA',
-                'choice':'scissor',
-                'player_room_id':player_room_id});
-
-        }else {
-        socket.emit('register_player_choice', {
-            'player_number': player_number,
-            'player1': player1,
-            'player2': player2,
-            'choice': 'scissor',
-            'player_room_id': player_room_id
-        });
-        }
+      handleChoiceClick('scissor');
+    }
+  
+    /**
+     * Handle choice click events.
+     *
+     * @param {string} choice - Choice of the player ('rock', 'paper', or 'scissor').
+     */
+    function handleChoiceClick(choice) {
+      const playerNumber = (player1 === username) ? 'player1' : 'player2';
+  
+      setChoiceImage(playerNumber, choice);
+  
+      const payload = {
+        player_number: playerNumber,
+        player1,
+        player2: maria ? 'MarIA' : player2,
+        choice,
+        player_room_id: playerRoomId
+      };
+  
+      socket.emit('register_player_choice', payload);
     }
 });
+    
